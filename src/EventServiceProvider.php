7b91950528f8aaaacb5a5298b796eeb5352c710a
glebzaveruha <gzaveruha@best4u.md>
2018-02-05 18:45:31 +0200

    TimelineEvent Listener

--- src/EventServiceProvider.php
+++ src/EventServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modules\Logs\Providers;
+namespace B4u\TimelineModule;
 
 use B4u\TimelineModule\Listeners\TimelineEventListener;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
